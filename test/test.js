8bfbbd9c1e79a3e6e41236f33e57049d45e69e5d
Adil GUEZZI <aguezzi@Adils-MacBook-Pro.local>
2017-12-15 10:42:56 +0100

    red: add case for 2 and 3

--- test/test.js
+++ test/test.js
@@ -8,4 +8,10 @@ describe('FizzBuzz Kata Test: \n', () => {
     it('should return I when input is 1', () => {
         expect(romanNumber(1)).to.equal('I');
     });
+    it('should return II when input is 2', () => {
+        expect(romanNumber(2)).to.equal('II');
+    });
+    it('should return III when input is 3', () => {
+        expect(romanNumber(3)).to.equal('III');
+    });
 });
