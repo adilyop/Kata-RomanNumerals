a8c563964d41bccafba85f07b19b3ad0bee02112
Adil GUEZZI <aguezzi@Adils-MacBook-Pro.local>
2017-12-15 10:27:06 +0100

    first commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('FizzBuzz Kata Test: \n', () => {
+    it('should return I when input is 1', () => {
+        expect(1).to.equal('I');
+    });
+});
